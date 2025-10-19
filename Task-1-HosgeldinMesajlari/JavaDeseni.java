fa2502afae47ab5c412fecb5988731525d67dc9b
cemile871 <250541099@firat.edu.tr>
2025-10-20 00:05:12 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,10 @@
+public class Main  {
+    public static void main (String[] args)
+    {
+        System.out.println("      J A V V A");
+        System.out.println("    J A A V V A A ");
+        System.out.println("  J J AAAA V V AAAA");
+        System.out.println("    J A A V V A A ");
+        System.out.println("      J A V V A ");
+    }
+}
