a7ed02908d3bd35f03d4dfab1c9283dc97ca469a
cemile871 <250541099@firat.edu.tr>
2025-10-20 18:50:27 +0300

    Update JavaDeseni.java

--- Task-1-HosgeldinMesajlari/JavaDeseni.java
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -1,10 +1,11 @@
-public class Main  {
-    public static void main (String[] args)
-    {
-        System.out.println("      J A V V A");
-        System.out.println("    J A A V V A A ");
-        System.out.println("  J J AAAA V V AAAA");
-        System.out.println("    J A A V V A A ");
-        System.out.println("      J A V V A ");
-    }
-}
+<?xml version="1.0" encoding="UTF-8"?>
+<module type="JAVA_MODULE" version="4">
+  <component name="NewModuleRootManager" inherit-compiler-output="true">
+    <exclude-output />
+    <content url="file://$MODULE_DIR$">
+      <sourceFolder url="file://$MODULE_DIR$/src" isTestSource="false" />
+    </content>
+    <orderEntry type="inheritedJdk" />
+    <orderEntry type="sourceFolder" forTests="false" />
+  </component>
+</module>
