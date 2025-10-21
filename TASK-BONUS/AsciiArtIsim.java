88597419e8e813bc7dadbc90ff7732b31ee3f263
cemile871 <250541099@firat.edu.tr>
2025-10-22 00:15:07 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,28 @@
+public class AsciiArtIsim {
+
+    public static void main(String[] args) {
+
+        String ad = "CEMILE";
+
+        System.out.println("***********************************");
+        System.out.println("          " + ad + " ADININ ASCII ART'I    ");
+        System.out.println("***********************************");
+
+        // 1. SATIR
+        System.out.println(" _____  _____  __  __  _____  __    _____ ");
+        // 2. SATIR
+        System.out.println("/  __ \\|  ___||  \\/  ||_   _||  |  | ____|");
+        // 3. SATIR
+        System.out.println("| /  \\/| |__  | .  . |  | |  | |  | |__  ");
+        // 4. SATIR
+        System.out.println("| |    |  __| | |\\/| |  | |  | |  | ___| ");
+        // 5. SATIR
+        System.out.println("| \\__/\\| |___ | |  | |  | |  | |  | |___ ");
+        // 6. SATIR
+        System.out.println(" \\____/\\____/ |_|  |_|  \\_/  |_|  |____/ ");
+
+        System.out.println("\n***********************************");
+        System.out.println("       (Ad: " + ad + " )");
+        System.out.println("***********************************");
+    }
+}
