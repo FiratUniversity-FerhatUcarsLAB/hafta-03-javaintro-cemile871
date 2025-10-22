5a60169eae3463b41419b27521141973725a774b
cemile871 <250541099@firat.edu.tr>
2025-10-22 19:08:21 +0300

    Update IsimKarti.java

--- Task-4-StringDesenler/IsimKarti.java
+++ Task-4-StringDesenler/IsimKarti.java
@@ -1,15 +1,18 @@
 public class IsimKarti {
 
     public static void main(String[] args) {
+        // Kendi Bilgilerinizi Buraya Girin
         String ad = "Cemile";
         String soyad = "Akay";
         String ogrenciNo = "250541099";
         String bolum = "Yazılım Muhendisligi";
 
+        // Güzelleştirilmiş formatta ekrana yazdırma
         System.out.println("=========================================");
         System.out.println("             ÖĞRENCİ KİMLİK KARTI        ");
         System.out.println("=========================================");
 
+        // printf ile formatlı çıktı: %-15s sola hizalı 15 karakter
         System.out.printf("| %-15s : %-20s |\n", "Ad Soyad", ad + " " + soyad);
         System.out.printf("| %-15s : %-20s |\n", "Öğrenci No", ogrenciNo);
         System.out.printf("| %-15s : %-20s |\n", "Bölüm", bolum);
