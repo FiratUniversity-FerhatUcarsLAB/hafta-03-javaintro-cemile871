396d729ef77f13382d137133d8d313b90c753b65
cemile871 <250541099@firat.edu.tr>
2025-10-22 19:26:46 +0300

    Update CarpimTablosu.java

--- Task-5-TabloDesenleri/CarpimTablosu.java
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -9,11 +9,14 @@ public class CarpimTablosu {
         System.out.println("      " + carpan + "'NİN ÇARPIM TABLOSU     ");
         System.out.println("=================================");
 
+        // Satır formatı: "2 x 1 = 2" şeklinde çıktı almak için
+        // %d: Tam sayı
         String rowFormat = "%2d x %-2d = %2d\n";
 
         for (int i = 1; i <= sinir; i++) {
             int sonuc = carpan * i;
 
+            // %2d: Sağa hizalı, 2 karakter genişliğinde tam sayı
             System.out.printf(rowFormat, carpan, i, sonuc);
         }
 
