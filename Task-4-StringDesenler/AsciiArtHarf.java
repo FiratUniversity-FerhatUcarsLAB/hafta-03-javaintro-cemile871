47e35ab7e15ad7582bdc71faa874b44c1a0c4814
cemile871 <250541099@firat.edu.tr>
2025-10-22 19:14:18 +0300

    Update AsciiArtHarf.java

--- Task-4-StringDesenler/AsciiArtHarf.java
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -1,18 +1,20 @@
 public class AsciiArtHarf {
 
     public static void main(String[] args) {
-    
-        System.out.println("***********************************");
-        System.out.println("        BAŞ HARF ASCII ART         ");
-        System.out.println("***********************************");
-
-        System.out.println("C  C");
-        System.out.println("C C ");
-        System.out.println("CC  ");
-        System.out.println("C C ");
-        System.out.println("C  C");
 
         System.out.println("***********************************");
+        System.out.println("          BAŞ HARF ASCII ART         ");
+        System.out.println("***********************************");
+
+        // 'C' harfinin ASCII Art çizimi
+        System.out.println("  _____");
+        System.out.println(" / ____|");
+        System.out.println("| |    ");
+        System.out.println("| |    ");
+        System.out.println("| \\____");
+        System.out.println(" \\_____|");
+
+        System.out.println("\n***********************************");
         System.out.println("     (Baş harf: C)               ");
         System.out.println("***********************************");
     }
